07afd9b67376c5e4416e3acc80e0fc686492d79c
RedTeamNW <75023937+RedTeamNW@users.noreply.github.com>
2023-11-14 14:33:16 +0100

    Update vulnservice.ps1

--- vulnservice.ps1
+++ vulnservice.ps1
@@ -1,6 +1,8 @@
-mkdir C:\Myservice\
-Invoke-WebRequest https://github.com/RedTeamNW/VulnerableService/raw/master/vulnservice.exe -outfile c:\myservice\myservice.exe
-New-Service -Name MyService -BinaryPathName C:\Myservice\myservice.exe -Description MyService -DisplayName MyService -StartupType Automatic
-cmd /c "icacls C:\Myservice\myservice.exe /t /grant Everyone:(OI)(CI)F"
-cmd /c "sc sdset MyService D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;WD)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)"
-start-service MyService
+function Install-Service {
+  mkdir C:\Myservice\
+  Invoke-WebRequest https://github.com/RedTeamNW/VulnerableService/raw/master/vulnservice.exe -outfile c:\myservice\myservice.exe
+  New-Service -Name MyService -BinaryPathName C:\Myservice\myservice.exe -Description MyService -DisplayName MyService -StartupType Automatic
+  cmd /c "icacls C:\Myservice\myservice.exe /t /grant Everyone:(OI)(CI)F"
+  cmd /c "sc sdset MyService D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;WD)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)"
+  start-service MyService
+}
